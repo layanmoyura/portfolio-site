1ef2195ab5b4b59417e11f9fbe60219f01f582eb
layanmoyura <layanmoyura@gmail.com>
2024-01-07 12:05:42 +0530

    10th mod

--- mail/contact.php
+++ mail/contact.php
@@ -1,7 +1,7 @@
 <?php
-if(empty($_POST['name']) || empty($_POST['subject']) || empty($_POST['message']) || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
-  http_response_code(500);
-  exit();
+if ($_SERVER["REQUEST_METHOD"] !== "POST" || empty($_POST['name']) || empty($_POST['subject']) || empty($_POST['message']) || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+    http_response_code(500);
+    exit();
 }
 
 $name = strip_tags(htmlspecialchars($_POST['name']));
@@ -9,12 +9,14 @@ $email = strip_tags(htmlspecialchars($_POST['email']));
 $m_subject = strip_tags(htmlspecialchars($_POST['subject']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
 
-$to = "info@example.com"; // Change this email to your //
-$subject = "$m_subject:  $name";
-$body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\n\nEmail: $email\n\nSubject: $m_subject\n\nMessage: $message";
-$header = "From: $email";
-$header .= "Reply-To: $email";	
+$to = "layanmoyura@gmail.com";
+$subject = "$m_subject: $name";
+$body = "You have received a new message from your website contact form.\n\n" . "Here are the details:\n\nName: $name\n\nEmail: $email\n\nSubject: $m_subject\n\nMessage: $message";
+$header = "From: $email\r\n";
+$header .= "Reply-To: $email\r\n";
 
-if(!mail($to, $subject, $body, $header))
-  http_response_code(500);
+if (!mail($to, $subject, $body, $header)) {
+    http_response_code(500);
+    exit();
+}
 ?>
