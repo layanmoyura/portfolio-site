ed06311f8a59578abf0c6446fa1633046070437e
layanmoyura <layanmoyura@gmail.com>
2024-01-07 18:41:45 +0530

    11th mod

--- mail/contact.php
+++ mail/contact.php
@@ -1,22 +1,29 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] !== "POST" || empty($_POST['name']) || empty($_POST['subject']) || empty($_POST['message']) || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
-    http_response_code(500);
-    exit();
-}
 
-$name = strip_tags(htmlspecialchars($_POST['name']));
-$email = strip_tags(htmlspecialchars($_POST['email']));
-$m_subject = strip_tags(htmlspecialchars($_POST['subject']));
-$message = strip_tags(htmlspecialchars($_POST['message']));
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Allow-Headers: Content-Type");
 
-$to = "layanmoyura@gmail.com";
-$subject = "$m_subject: $name";
-$body = "You have received a new message from your website contact form.\n\n" . "Here are the details:\n\nName: $name\n\nEmail: $email\n\nSubject: $m_subject\n\nMessage: $message";
-$header = "From: $email\r\n";
-$header .= "Reply-To: $email\r\n";
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $subject = $_POST["subject"];
+    $message = $_POST["message"];
 
-if (!mail($to, $subject, $body, $header)) {
-    http_response_code(500);
-    exit();
+    // You can customize the filename and path as needed
+    $file = "form_data.txt";
+
+    // Prepare the data to be written to the text file
+    $data = "Name: $name\nEmail: $email\nSubject: $subject\nMessage: $message\n\n";
+
+    // Save the data to the text file
+    file_put_contents($file, $data, FILE_APPEND | LOCK_EX);
+
+    // Send a success response to the AJAX request
+    echo "success";
+} else {
+    // Send an error response if the request method is not POST
+    http_response_code(400);
+    echo "Bad Request";
 }
 ?>
